22d82be6844bd91ca8f5ef9a8e7e6895ee4089cb
danielvasic <dvasic@gmail.com>
2021-04-20 10:18:09 +0200

    Pobrisano

--- DrugiOOPProjekt/src/Program.java
+++
@@ -1,9 +0,0 @@
-public class Program {
-    public static void main(String [] args){
-        Tocka t1 = new Tocka();
-        Tocka t2 = new Tocka(1,0);
-        System.out.println(t1.equals(t1));
-        double udaljenost = t1.udaljenost(t2);
-        System.out.println(udaljenost);
-    }
-}
